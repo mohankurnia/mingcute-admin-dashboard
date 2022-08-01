c0a2a90cefa786b0aba79b4691958a6353083275
Mohan Kurnia <handana1804@gmail.com>
2022-08-01 15:58:56 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Performance Chart
+const performanceLabels = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat'
+];
+
+const performanceData = {
+  labels: performanceLabels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [135, 80, 98, 45, 60, 145, 80],
+  },
+  {
+    label: 'My Second dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [160, 55, 120, 50, 55, 157, 55],
+  }]
+};
+
+const performanceConfig = {
+  type: 'bar',
+  data: performanceData,
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      },
+      scales: {
+        y: {
+          ticks: {
+            
+          }
+        }
+      }
+    }
+  }
+};
+
+const performanceChart = new Chart(
+  document.getElementById('js-performance__chart'),
+  performanceConfig
+);
